3c89985fc622d35d6b0a73a6f032c886de491aa0
Pratham Sahai <divyakumar12a48@gmail.com>
2025-05-08 20:12:48 +0530

    first commit

---
+++ mycoding.java/addtwonum.java
@@ -0,0 +1,9 @@
+class addtwonum{
+    public static void main(String[] args) {
+        int a = 78;
+        int b = 90;
+        int c = a + b;
+        System.out.println("The sum of two num is"+c);
+        
+    }
+}
